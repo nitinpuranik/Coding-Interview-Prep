5645abac6af32d083c22f4c764f711ab8fb1c79f
Nitin Puranik <nitin.n.puranik@gmail.com>
2018-06-20 10:01:41 -0700

    Changed DisjointSet into a class

--- CodeSnippets/DisjointSet.cpp
+++ CodeSnippets/DisjointSet.cpp
@@ -9,98 +9,103 @@
 #include <unordered_map>
 using namespace std;
 
-typedef struct node {
-  int data;
-  unsigned rank;
-  struct node *parent;
-} Node;
+class DisjointSet {
+  private:
+    typedef struct node {
+      int data;
+      unsigned rank;
+      struct node *parent;
+    } Node;
 
-unordered_map<int, Node*> nodemap;
+    unordered_map<int, Node*> umap;
+    Node* FindSet (Node *node);
 
-void MakeSet (int data) {
+  public:
+    int FindSet (int data);
+    bool Union (int data1, int data2);
+    void MakeSet (int data);
+};
+
+void DisjointSet::MakeSet (int data) {
   Node *newnode = new Node;
-  
   newnode->data = data;
   newnode->rank = 0;
   newnode->parent = newnode;
-  
-  nodemap[data] = newnode;
+
+  umap[data] = newnode;
 }
 
-Node* FindSet (Node *node) {
+int DisjointSet::FindSet (int data) {
+  Node *node = umap[data];
+
+  if (node == nullptr) {
+    cout << data << ": Invalid parameter!" << endl;
+    return 0xdeadbeef;
+  }
+
+  Node *root = FindSet(node);
+
+  return root->data;
+}
+
+DisjointSet::Node* DisjointSet::FindSet (Node *node) {
   if (node == nullptr)
     return nullptr;
-  
-  if (node == node->parent)
+
+  if (node->parent == node)
     return node;
-  
+
   node->parent = FindSet (node->parent);
   return node->parent;
 }
 
-int FindSet (int data) {
-  unordered_map<int, Node*>::const_iterator it;
-  
-  it = nodemap.find(data);
-  
-  if (it != nodemap.end()) {
-    Node *root = FindSet (it->second);
-    
-    if (root)
-      return root->data;
-  }
-  
-  return 0xdeadbeef;
-}
+bool DisjointSet::Union (int data1, int data2) {
+  Node *node1 = umap[data1];
+  Node *node2 = umap[data2];
 
-bool Union (int data1, int data2) {
-  Node *node1 = nodemap[data1];
-  Node *node2 = nodemap[data2];
-  
-  if (node1 == nullptr || node2 == nullptr) {
-    cout << "Invalid input." << endl;
+  if (node1 == nullptr || node2 == nullptr)
     return false;
-  }
-  
-  Node *parent1 = FindSet(node1);
-  Node *parent2 = FindSet(node2);
-  
+
+  Node *parent1 = FindSet (node1);
+  Node *parent2 = FindSet (node2);
+
   if (parent1 == parent2)
     return false;
-  
+
   if (parent1->rank >= parent2->rank) {
     parent1->rank = (parent1->rank == parent2->rank) ?
-                        parent1->rank + 1 : parent1->rank;
-    
+                    parent1->rank + 1 : parent1->rank;
     parent2->parent = parent1;
   } else {
     parent1->parent = parent2;
   }
-  
+
   return true;
 }
 
 int main() {
-  MakeSet(1);
-  MakeSet(2);
-  MakeSet(3);
-  MakeSet(4);
-  MakeSet(5);
-  MakeSet(6);
-  MakeSet(7);
+  DisjointSet ds;
 
-  Union(1, 2);
-  Union(2, 3);
-  Union(4, 5);
-  Union(6, 7);
-  Union(5, 6);
-  Union(3, 7);
+  ds.MakeSet(1);
+  ds.MakeSet(2);
+  ds.MakeSet(3);
+  ds.MakeSet(4);
+  ds.MakeSet(5);
+  ds.MakeSet(6);
+  ds.MakeSet(7);
 
-  cout << FindSet(1) << endl;
-  cout << FindSet(2) << endl;
-  cout << FindSet(3) << endl;
-  cout << FindSet(4) << endl;
-  cout << FindSet(5) << endl;
-  cout << FindSet(6) << endl;
-  cout << FindSet(7) << endl;
+  ds.Union(1, 2);
+  ds.Union(2, 3);
+  ds.Union(4, 5);
+  ds.Union(6, 7);
+  ds.Union(5, 6);
+  ds.Union(3, 7);
+
+  cout << ds.FindSet(1) << endl;
+  cout << ds.FindSet(2) << endl;
+  cout << ds.FindSet(3) << endl;
+  cout << ds.FindSet(4) << endl;
+  cout << ds.FindSet(5) << endl;
+  cout << ds.FindSet(6) << endl;
+  cout << ds.FindSet(7) << endl;
 }
