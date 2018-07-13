7b543319f867848a85a31bb32429f670c4091928
Nitin Puranik <nitin.n.puranik@gmail.com>
2018-07-12 19:37:16 -0700

    Array problems C++ port

--- CodeSnippets/Arr_MaxNonContigSum.cpp
+++ CodeSnippets/Arr_MaxNonContigSum.cpp
@@ -1,55 +1,56 @@
 // If all negatives, then pick the least negative number.
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-void PrintMax (int *arr, int index, int *pred) {
+void PrintMax (const auto& arr, int index, const auto& pred) {
   if (pred[index] == -1) {
-    printf("%d", arr[index]);
-  } else {
-    PrintMax (arr, pred[index], pred);
-    printf(" + %d", arr[index]);
-  }
-}
-
-void MaxSum (int *arr, int n) {
-  int maxsum, maxindex;
-  int pred[n], i;
-  
-  if (arr == NULL || n <= 0) {
+    cout << arr[index];
     return;
   }
   
-  for (i = 0; i < n; i++) {
-    pred[i] = -1;
-  }
+  PrintMax (arr, pred[index], pred);
+  cout << " + " << arr[index];
+}
+
+void MaxNonContigSum (const auto& arr) {
+  if (arr.empty())
+    return;
   
-  maxsum = arr[0];
-  maxindex = 0;
+  int maxsum = arr[0];
+  int maxindex = 0;
   
-  for (i = 1; i < n; i++) {
+  vector<int> pred {-1};
+  
+  for (unsigned i = 1; i < arr.size(); i++) {
+    
+    pred.push_back(-1);
+
     if (arr[i] > 0) {
+      
       if (maxsum > 0) { // If maxsum is positive, then adding arr[i] improves things.
-        pred[i] = maxindex;
         maxsum += arr[i];
+        pred[i] = maxindex;
       } else { // If maxsum is negative, restart the sequence from this positive element.
         maxsum = arr[i];
       }
       
       maxindex = i;
-      
+
     } else if (arr[i] > maxsum) { // If arr[i] is negative, then check if it is still greater than maxsum.
       maxsum = arr[i];
       maxindex = i;
     }
   }
   
-  PrintMax (arr, maxindex, pred);
+  PrintMax(arr, maxindex, pred);
 }
 
 int main() {
-  int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+  vector<int> arr {-2, -3, 4, -1, -2, 1, 5, -3};
   
-  MaxSum(arr, sizeof(arr)/sizeof(int));
+  MaxNonContigSum(arr);
   
   return 0;
 }
