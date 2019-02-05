4a1e63b3f1bde25cc0d442376294f275864b47ff
Nitin Puranik <nitin.n.puranik@gmail.com>
2019-02-04 16:37:27 -0800

    Circular Buffer Improvements

--- CodeSnippets/CircularBuffer.cpp
+++ CodeSnippets/CircularBuffer.cpp
@@ -1,42 +1,49 @@
+/*
+ * Given a buffer with indices that are offsets from the current index,
+ * determine whether hopping through the indices will take you through
+ * the buffer full circle ending at index 0. For example, if arr[0] = 2,
+ * it means you need to hop 2 indices further. If arr[2] = -1, then go
+ * back one index. If arr[0] = -1, then wrap back and go to the last index.
+ * So an array such as {2,2,-1} is a circular array while {2,1,-1} is not.
+ */
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
 using namespace std;
 
-void IsCircular (const vector<int>& arr) {
+bool IsCircular (const vector<int>& arr) {
   unordered_set<int> visited;
+
   int index = 0;
-  
-  while (true) {
-    if (visited.find(index) == visited.end()) {
-      visited.insert(index);
-      
-      index = index + arr[index];
-      
-      if (index >= (int) arr.size()) {
-        index %= arr.size();
-      } else if (index < 0) {
-        index *= -1;
-        index = arr.size() - (index % arr.size());
-        
-        index = index == (int) arr.size() ? 0 : index;
-      }
-    } else {
-      if (index == 0 && visited.size() == arr.size()) {
-        cout << "Circular buffer." << endl;
-      } else {
-        cout << "Not a circular buffer." << endl;
-      }
-      
-      return;
+
+  visited.insert(0);
+
+  while(true) {
+    index += arr[index];
+
+    if (index < 0) {
+      index = (index * (-1)) % arr.size();
+      index = arr.size() - index;
     }
+
+    index %= arr.size();
+
+    if (visited.find(index) != visited.end())
+      return index == 0;
+
+    visited.insert(index);
   }
 }
 
 int main() {
-  vector<int> arr {-7, -10, -1};
-  
-  IsCircular (arr);
-  
+  vector<int> arr {2,-1,-1};
+  //vector<int> arr {-7, -10, -1};
+
+  if (IsCircular (arr))
+    cout << "Circular buffer." << endl;
+  else
+    cout << "Not a circular buffer." << endl;
+
   return 0;
 }
