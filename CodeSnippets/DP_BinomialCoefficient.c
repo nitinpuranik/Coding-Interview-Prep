3a837e7a7b64ecc56e456aa1ea08a9214b356ec0
Nitin Puranik <nitin.n.puranik@gmail.com>
2019-02-05 16:40:10 -0800

    Improvements

--- CodeSnippets/DP_BinomialCoefficient.c
+++ CodeSnippets/DP_BinomialCoefficient.c
@@ -1,32 +1,34 @@
-#include <stdio.h>
+#include <iostream>
+#include <array>
+using namespace std;
 
-unsigned long Triangulate(unsigned int n, unsigned int k) {
-  unsigned long Pascal[n + 1][k + 1];
-  unsigned int row, col;
+/*
+ * Find the combinatorial solution of nCk using
+ * binomial method. This serves as a nice introduction
+ * to the idea of dynamic programming.
+ */
 
-  /* Initialize the row base case. */
-  for (row = 0; row <= n; row++) {
-    Pascal[row][0] = 1;
-  }
+int Triangulate(int n, int k) {
+  int arr[n + 1][k + 1];
 
-  /* Initialize the column base case. */
-  for (col = 0; col <= k; col++) {
-    Pascal[col][col] = 1;
-  }
+  for (int row = 0; row <= n; row++)
+    arr[row][0] = 1;
 
-  /* Compute the combination now. */
-  for (row = 2; row <= n; row++) {
-    for(col = 1; col < row; col++) {
-      Pascal[row][col] = Pascal[row - 1][col - 1] + Pascal[row - 1][col];
+  for (int col = 1; col <= k; col++)
+    arr[col][col] = 1;
+
+  for (int row = 2; row <= n; row++) {
+    for (int col = 1; col < row; col++) {
+      arr[row][col] = arr[row - 1][col - 1] + arr[row - 1][col];
     }
   }
-  
-  return Pascal[n][k];
+
+  return arr[n][k];
 }
 
 int main() {
-  unsigned int n = 5,k = 3;
-  
-  printf("%uC%u: %lu\n", n,k, Triangulate(n,k));
+  int n = 5, k = 3;
+
+  cout << n << "C" << k << " = " << Triangulate(n,k) << endl;
   return 0;
 }
--- CodeSnippets/DP_LongestCommonSubsequence.c
+++ CodeSnippets/DP_LongestCommonSubsequence.c
@@ -2,21 +2,27 @@
 #include <vector>
 using namespace std;
 
+/*
+ * Algorithm: Take two strings - say abcm and acm. Check if the last two characters
+ * are the same. If they are, then the length of the subsequence is the LCS of the two
+ * strings without the last string, plus one. That is, LCS(abcm, acm) = LCS(abc, ac) + 1.
+ * If the last two characters don't match, LCS(abck, acm) = max(LCS(abck, ac), LCS(abc, acm)).
+ */
+
 void PrintLCS (const string& s1, const string& s2, auto const& tbl, int row, int col) {
   if (tbl[row][col]) {
-    if (s2[row - 1] == s1[col - 1]) {
+    if (s1[row - 1] == s2[col - 1]) {
       PrintLCS (s1, s2, tbl, row - 1, col - 1);
-      cout << s2[row - 1];
+      cout << s1[row - 1];
+    } else if (tbl[row - 1][col] > tbl[row][col - 1]) {
+      PrintLCS (s1, s2, tbl, row - 1, col);
     } else {
-      if (tbl[row - 1][col] > tbl[row][col - 1])
-        PrintLCS (s1, s2, tbl, row - 1, col);
-      else
-        PrintLCS (s1, s2, tbl, row, col - 1);
+      PrintLCS (s1, s2, tbl, row, col - 1);
     }
   }
 }
 
-// s1 along cols, s2 along rows
+// s1 along rows, s2 along rows
 void LCS (const string& s1, const string& s2) {
   if (s1.empty() || s2.empty()) {
     cout << "Empty string" << endl;
@@ -25,27 +31,52 @@ void LCS (const string& s1, const string& s2) {
 
   vector<vector<int>> tbl;
 
-  for (unsigned i = 0; i <= s2.length(); i++)
+  for (unsigned i = 0; i <= s1.length(); i++)
     tbl.push_back({0});
 
-  for (unsigned i = 1; i <= s1.length(); i++)
+  for (unsigned i = 1; i <= s2.length(); i++)
     tbl[0].push_back(0);
 
-  for (unsigned i = 1; i <= s2.length(); i++) {
-    for (unsigned j = 1; j <= s1.length(); j++) {
-      if (s2[i - 1] == s1[j - 1]) {
+  for (unsigned i = 1; i <= s1.length(); i++) {
+    for (unsigned j = 1; j <= s2.length(); j++) {
+      if (s1[i - 1] == s2[j - 1]) {
         tbl[i].push_back(tbl[i - 1][j - 1] + 1);
       } else {
-        int entry = tbl[i - 1][j] > tbl[i][j - 1] ? tbl[i - 1][j] : tbl[i][j - 1];
+        int entry = tbl[i - 1][j] > tbl[i][j - 1] ?
+                    tbl[i - 1][j] : tbl[i][j - 1];
         tbl[i].push_back(entry);
       }
     }
   }
 
-  if (tbl[s2.length()][s1.length()] == 0)
+  if (tbl[s1.length()][s2.length()] == 0)
     cout << "No LCS" << endl;
 
-  PrintLCS (s1, s2, tbl, tbl.size() - 1, tbl[0].size() - 1);
+  PrintLCS (s1, s2, tbl, s1.length(), s2.length());
+  cout << endl;
+
+  /*
+   * If you'd like to implement your own stack, instead of the
+   * runtime's recursion infrastructure, here is the code snippet.
+   */
+  vector<char> subseq;
+
+  int row = s1.length();
+  int col = s2.length();
+
+  while (tbl[row][col]) {
+    if (s1[row - 1] == s2[col - 1]) {
+      subseq.push_back(s1[row - 1]);
+      row--; col--;
+    } else if (tbl[row][col - 1] > tbl[row - 1][col]) {
+      col--;
+    } else {
+      row--;
+    }
+  }
+
+  for (vector<char>::const_reverse_iterator it = subseq.rbegin(); it != subseq.rend(); it++)
+    cout << *it;
 }
 
 int main() {
