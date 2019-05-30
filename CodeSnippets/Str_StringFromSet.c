c24c59060f338f46bba0130016f1cf172ecfa965
Puranik <nitinp@netapp.com>
2019-05-30 09:38:52 -0700

    Hard Problems

--- CodeSnippets/Str_StringFromSet.c
+++ CodeSnippets/Str_StringFromSet.c
@@ -2,28 +2,32 @@
 #include <vector>
 using namespace std;
 
-bool FooWorker (const auto& arr, const string& target, unsigned index) {
+bool Foo (const vector<string>& arr, const string& target) {
+  static unsigned index = 0;
+
+  if (arr.empty() || target.empty())
+    return false;
+
+  if (index == target.length())
+    return true;
+
   for (const string& str : arr) {
 
-    if (target[index] == str[0]) {
+    if (str[0] == target[index]) {
 
-      if (target.find(str, index) == index) {
+      if (target.find (str, index) == index) {
 
-        if (index + str.length() == target.length())
+        index += str.length();
+
+        if (Foo (arr, target) == true)
           return true;
 
-        if (true == FooWorker (arr, target, index + str.length())) {
-          return true;
-        }
+        index -= str.length();
       }
     }
   }
 
   return false;
-}
-
-bool Foo (const auto& arr, const string& target) {
-  return FooWorker (arr, target, 0);
 }
 
 int main () {
